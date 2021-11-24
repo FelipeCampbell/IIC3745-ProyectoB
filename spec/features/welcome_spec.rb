ea0a8b18bcc78d9b35ef9b15396295c5678c1761
matiasop <movalle4@uc.cl>
2021-11-24 20:03:56 -0300

    test(create-movies): expect page to have created movie

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -15,10 +15,19 @@ RSpec.describe 'Create-Movie', type: :system do
     fill_in "movie_image", :with => "https://i.redd.it/nvaxpuqwius41.jpg"
     fill_in "movie_planners_attributes_0_start_date", :with => "12/01/2021"
     fill_in "movie_planners_attributes_0_end_date", :with => "12/10/2021"
-    select "Room 3", from: "movie_planners_attributes_0_room"
+    select "Room 6", from: "movie_planners_attributes_0_room"
     select "Noche", from: "movie_planners_attributes_0_time"
     click_link "Add Another Room/Time"
     click_button "Create"
+
+    # Check movie
+    expect(page).to have_content("test-movie-name")
+    expect(page).to have_content("2021-12-01")
+    expect(page).to have_content("2021-12-10")
+    expect(page).to have_content("6") # Room 6
+    expect(page).to have_content("3") # Noche
+    expect(page).to have_content("1") # Room 1
+    expect(page).to have_content("1") # Matiné
   end
 
   after do
