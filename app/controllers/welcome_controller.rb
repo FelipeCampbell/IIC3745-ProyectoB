52e1588e86381479b2b8927ec29e921de489bd23
Cristian Hinostroza <cahinostroza@uc.cl>
2021-11-24 21:24:37 -0300

    create seats component

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,22 @@
 class WelcomeController < ApplicationController
-  def index
+
+  def index 
+    if $clicked_seats
+      add_or_remove_clicked_seat
+    else
+      $clicked_seats = []
+    end
   end
+
+  def add_or_remove_clicked_seat
+    if params[:row] and params[:column]
+      clicked_seat = {row:params[:row], column:params[:column].to_i}
+      if $clicked_seats.include?(clicked_seat)
+        $clicked_seats.delete(clicked_seat)
+      else
+        $clicked_seats << clicked_seat
+      end
+    end
+  end
+
 end
