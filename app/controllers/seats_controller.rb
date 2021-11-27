b22f1cf1440b0d05636c08eca973310fc54614a8
Alejandra Uribe <anuribe@uc.cl>
2021-11-27 09:29:36 -0300

    Add create seats function to controller

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -12,6 +12,16 @@ class SeatsController < ApplicationController
     end
   end
 
+  def create
+    mid, room, time = params[:movie_id], params[:room_id], (["Matiné", "Tanda", "Noche"].find_index(params[:time_id])+1)
+    screening = Screening.where(movie_id: mid, room: room.to_i, time: time).first
+    $clicked_seats.each do |seat|
+      Seat.create(col: seat[:col], row: seat[:row], screening_id: screening.id)
+    end
+    $clicked_seats = []
+    redirect_to view_seats_path(mid, room, time)
+  end
+
   def add_or_remove_clicked_seat
     if params[:row] and params[:col]
       clicked_seat = {row: params[:row], col: params[:col].to_i}
