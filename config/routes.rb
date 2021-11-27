c1f36046c8998269f5d63e7bf62c7d915e75c405
Alejandra Uribe <anuribe@uc.cl>
2021-11-26 21:45:59 -0300

    Add final route with params to buy seats

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :movies
-  get 'welcome/index'
+  get 'seats/movie_id=:movie_id&room=:room_id&time=:time_id', to: "seats#index", as: :buy_seats
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
