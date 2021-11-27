752baabf5c3001edf4e0e8b1af002a4265dcd285
Alejandra Uribe <anuribe@uc.cl>
2021-11-27 09:01:48 -0300

    Add post route to create a new seat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :movies
-  get 'seats/movie_id=:movie_id&room=:room_id&time=:time_id', to: "seats#index", as: :buy_seats
+  get 'seats/movie_id=:movie_id&room=:room_id&time=:time_id', to: "seats#index", as: :view_seats
+  post 'seats/movie_id=:movie_id&room=:room_id&time=:time_id', to: "seats#create", as: :buy
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
 end
