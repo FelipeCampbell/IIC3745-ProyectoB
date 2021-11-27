b75825193ddc786b2ec96c736a40dae3a66cf1b8
Alejandra Uribe <anuribe@uc.cl>
2021-11-27 09:30:14 -0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Movie.create(name: 'M1', image: '2')
+Screening.create(room: 1, date: DateTime.new(2015, 6, 22), time: 1, movie_id: 1)
+Seat.create(col: 1, row: "B", screening_id: 1)
+Seat.create(col: 1, row: "C", screening_id: 1)
+Seat.create(col: 2, row: "B", screening_id: 1)
+Seat.create(col: 3, row: "B", screening_id: 1)
